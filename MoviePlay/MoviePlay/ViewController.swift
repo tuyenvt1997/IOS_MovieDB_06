36f397b7bd0726c0c2c75ce66d22b1d4bd85c33e
tuyenvt1997 <tuyenvtk14ktqs@gmail.com>
2018-09-21 10:22:09 +0700

    init project 1

---
+++ MoviePlay/MoviePlay/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MoviePlay
+//
+//  Created by tuyente on 9/21/18.
+//  Copyright © 2018 tuyente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
